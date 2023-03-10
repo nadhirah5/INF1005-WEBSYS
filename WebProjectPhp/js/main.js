a8a4442b1f1d2c6afaad1e82c0b9d9e3ca43696c
nadhirah5 <nadhirahayubkhan@gmail.com>
2023-03-10 11:38:10 +0800

    updated main.js for nav bar

--- WebProjectPhp/js/main.js
+++ WebProjectPhp/js/main.js
@@ -1,6 +1,26 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+
+$(document).ready(function() {
+  activateMenu();
+});
+
+
+
+
+/*
+ * This function toggles the nav menu active/inactive status as
+ * different pages are selected. It should be called from $(document).ready()
+ * or whenever the page loads.
  */
-
-
+function activateMenu()
+{
+    var current_page_URL = location.href;
+    $(".navbar-nav a").each(function()
+    {
+        var target_URL = $(this).prop("href");
+        if (target_URL === current_page_URL)
+        {
+            $('nav a').parents('li, ul').removeClass('active');
+            $(this).parent('li').addClass('active');
+            return false;
+} });
+}
